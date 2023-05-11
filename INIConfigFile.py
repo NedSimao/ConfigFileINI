82905cfd5b7b5b9efb855d342572904a5600ca05
Nedved SIMAO <109746899+NedSimao@users.noreply.github.com>
2023-05-11 22:25:08 +0200

    Definition of my INI Config Class Model; Now it is time to start implementing

---
+++ INIConfigFile.py
@@ -0,0 +1,32 @@
+class IniFile():
+
+    def __init__(self):
+        pass
+
+
+    def OpenConfigData(self, path):
+        pass
+
+    def readKey(self, keyName):
+        pass
+
+    def writeKey(self, keyName):
+        pass
+
+    def removeKey(self, keyName):
+        pass
+
+    def removeSection(self, keyName):
+        pass
+
+    def CloseConfigData(self):
+        pass
+
+    def getKeyNames(self, keyName):
+        pass
+
+    def getSectionNames(self, keyName):
+        pass
+
+    def notConfigDataPath(self, sectionName):
+        pass
